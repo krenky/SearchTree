9545b44dacbf12d0fbe840a8c656da022fbcdcd6
Strange <nazaroff.serezha2014@gmail.com>
2021-03-25 17:59:11 +0300

    Succes

---
+++ SearchTree/SearchTree/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace SoloLearn
+{
+    class BST
+    {
+        static void Main(string[] args)
+        {
+            SearchTree t = new SearchTree();
+            int i = new int();
+            /*t.insert(5);
+            t.insert(3);
+            t.insert(8);
+            t.insert(1);
+            t.insert(4);
+            t.insert(7);
+            t.insert(9);
+            t.insert(0);
+            t.insert(2);
+            t.insert(6);
+            t.insert(10);
+            t.search(t.root, 11);
+            DeleteNode(5, t.root);
+            t.print(Console.WindowWidth / 2, 0, t.root, 0);*/
+            while (i < 10)
+            {
+                Console.WriteLine("1:add " +
+                                  "\n2:print" +
+                                  "\n3:delete" +
+                                  "\n4:search");
+                i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        int NewItem = new int();
+                        Console.WriteLine("input new item");
+                        NewItem = Convert.ToInt32(Console.ReadLine());
+                        t.insert(NewItem);
+                        break;
+                    case 2:
+                        /*t.print(Console.WindowWidth / 2, 0, t.root, 0);
+                        Console.Read();
+                        Console.Write("\r ");*/
+                        t.Print(t.root);
+                        break;
+                    case 3:
+                        int DeleteItem = new int();
+                        Console.WriteLine("input item");
+                        DeleteItem = Convert.ToInt32(Console.ReadLine());
+                        t.DeleteNode(DeleteItem, t.root);
+                        break;
+                    case 4:
+                        int ItemSearch = new int();
+                        Console.WriteLine("input item");
+                        ItemSearch = Convert.ToInt32(Console.ReadLine());
+                        t.search(t.root, ItemSearch);
+                        break;
+                    case 5:
+                        t.print(Console.WindowWidth / 2, 0, t.root, 0);
+                        break;
+                    case 6:
+                        t.PrintOb(t.root);
+                        break;
+                    case 7:
+                        t.PrintOb(t.root);
+                        break;
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+    }
+}
+
---
+++ SearchTree/SearchTree/SearchTree.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace SoloLearn
+{
+    class SearchTree
+    {
+        public class Node
+        {
+            public int data;
+            public Node left, right;
+            public Node(int item)
+            {
+                data = item;
+                left = null;
+                right = null;
+            }
+        }
+        public Node root;
+        public SearchTree() //конструктор корня дерева
+        {
+            root = null;
+        }
+        public Node DeleteNode(int deleteData, Node root)
+        {
+            if (root == null)
+                return root;
+            if (deleteData < root.data)
+            {
+                root.left = DeleteNode(deleteData, root.left);
+            }
+            else if (deleteData > root.data)
+            {
+                root.right = DeleteNode(deleteData, root.right);
+            }
+            else if (root.left != null && root.right != null)
+            {
+                root.data = Minimum(root.right).data;
+                root.right = DeleteNode(root.data, root.right);
+                OnlyDelete(root);
+            }
+            else if (root.left != null)
+            {
+                return root.left;
+            }
+            else
+            {
+                return root.right;
+            }
+
+            return root;
+
+        }
+        static void OnlyDelete(Node root)
+        {
+            if (root.left != null)
+            {
+                OnlyDelete(root.left);
+            }
+            else
+            {
+                if (root.right == null)
+                {
+                    root = null;
+                }
+                else
+                {
+                    root = root.right;
+                }
+            }
+        }
+        static Node Minimum(Node root)
+        {
+            if (root != null)
+            {
+                if (root.left != null) root = Minimum(root.left);
+            }
+            return root;
+        }
+        public void Print(Node root)
+        {
+            if (root != null)
+            {
+                if (root.left != null)
+                {
+                    Print(root.left);
+                }
+
+                Console.WriteLine(root.data);
+                if (root.right != null)
+                {
+                    Print(root.right);
+                }
+            }
+        }
+        public void PrintPr(Node root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(root.data);
+                if (root.left != null)
+                {
+                    Print(root.left);
+                }
+
+
+                if (root.right != null)
+                {
+                    Print(root.right);
+                }
+            }
+        }
+        public void PrintOb(Node root)
+        {
+            if (root != null)
+            {
+                if (root.left != null)
+                {
+                    Print(root.left);
+                }
+
+
+                if (root.right != null)
+                {
+                    Print(root.right);
+                }
+                Console.WriteLine(root.data);
+            }
+        }
+        public void print(int x, int y, Node root, int delta)
+        {
+            if (root != null)
+            {
+                if (delta == 0) delta = x / 2;
+                Console.SetCursorPosition(x, y);
+                Console.Write(root.data);
+                print(x - delta, y + 3, root.left, delta / 2);
+                print(x + delta, y + 3, root.right, delta / 2);
+                Console.Clear();
+            }
+            return;
+        }
+        public Node search(Node root, int data)
+        {
+            if (root == null)
+            {
+                Console.Write("Not found\n");
+                return null;
+            }
+            if (root.data == data)
+            {
+                Console.Write("Found at: " + root.data + "\n");
+                return root;
+            }
+
+
+            if (root.data < data)
+            {
+                search(root.right, data);
+                return root;
+            }
+            else
+            {
+                search(root.left, data);
+                return root;
+            }
+        }
+        public void insert(int data)
+        {
+            root = recInsert(root, data);
+        }
+        Node recInsert(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return root;
+            }
+            if (data <= root.data)
+                root.left = recInsert(root.left, data);
+            else
+                root.right = recInsert(root.right, data);
+
+            return root;
+        }
+    }
+}
+
